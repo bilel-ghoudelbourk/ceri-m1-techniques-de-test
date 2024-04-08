931890181d88c1e91819e70cf98ba84671a83382
Bilel GHOUDELBOURK <gh23billo@gmail.com>
2024-04-08 15:18:24 +0200

    L'Ajout des test pour IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,25 +1,47 @@
 package fr.univavignon.pokedex.api;
-import static org.junit.Assert.assertNotNull;
+
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class IPokedexFactoryTest {
 
-    private IPokedexFactory pokedexFactory;
+    private PokedexFactory pokedexFactory;
     private IPokemonMetadataProvider metadataProvider;
     private IPokemonFactory pokemonFactory;
 
     @Before
     public void setUp() {
-        metadataProvider=new PokemonMetadataProvider();
+        metadataProvider = new PokemonMetadataProvider();
         pokemonFactory = new PokemonFactory(metadataProvider);
         pokedexFactory = new PokedexFactory(metadataProvider, pokemonFactory);
     }
 
     @Test
-    public void createPokedexTest() {
+    public void createPokedexWithNonNullParametersTest() {
         IPokedex createdPokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
-        assertNotNull(createdPokedex);
+        assertNotNull( createdPokedex);
     }
 
+    @Test
+    public void createPokedexWithNullParametersTest() {
+        IPokedex createdPokedex = pokedexFactory.createPokedex(null, null);
+        assertNotNull(createdPokedex);
+        // Further tests can be done to check if it's using the factory's default providers
+    }
+
+    @Test
+    public void testPokedexFunctionalities() throws PokedexException {
+        IPokedex pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        assertNotNull("Pokedex doit être creer", pokedex);
+
+        // Test adding a Pokemon and retrieving it to ensure Pokedex is functional
+        Pokemon testPokemon = pokemonFactory.createPokemon(0, 100, 100, 1000, 10);
+        int pokemonIndex = pokedex.addPokemon(testPokemon);
+        assertEquals("L'index du pokemon ajouté doit être 0", 0, pokemonIndex);
+
+        Pokemon pokemon = pokedex.getPokemon(pokemonIndex);
+        assertNotNull(pokemon);
+        assertEquals("pokemon il faut égale le pokemon ajouté", testPokemon, pokemon);
+    }
 }
