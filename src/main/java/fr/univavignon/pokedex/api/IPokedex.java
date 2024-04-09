7402a02e878f66e52b8c53d239f056adc4e86f05
Bilel GHOUDELBOURK <gh23billo@gmail.com>
2024-04-09 03:34:19 +0200

    javadoc problem solving

--- src/main/java/fr/univavignon/pokedex/api/IPokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedex.java
@@ -19,33 +19,33 @@ public interface IPokedex extends IPokemonMetadataProvider, IPokemonFactory {
 	int size();
 	
 	/**
-	 * Adds the given <tt>pokemon</tt> to this pokedex and returns
+	 * Adds the given <code>pokemon</code> to this pokedex and returns
 	 * it unique index.
-	 * 
+	 *
 	 * @param pokemon Pokemon to add to this pokedex.
 	 * @return Index of this pokemon relative to this pokedex.
 	 */
 	int addPokemon(Pokemon pokemon);
-	
+
 	/**
-	 * Locates the pokemon identified by the given <tt>id</tt>.
-	 * 
+	 * Locates the pokemon identified by the given <code>id</code>.
+	 *
 	 * @param id Unique pokedex relative identifier.
 	 * @return Pokemon denoted by the given identifier.
-	 * @throws PokedexException If the given <tt>index</tt> is not valid.
+	 * @throws PokedexException If the given <code>index</code> is not valid.
 	 */
 	Pokemon getPokemon(int id) throws PokedexException;
-	
+
 	/**
 	 * Returns an unmodifiable list of all pokemons this pokedex contains.
-	 * 
+	 *
 	 * @return Unmodifiable list of all pokemons.
 	 */
 	List<Pokemon> getPokemons();
 
 	/**
 	 * Returns an unmodifiable list of all pokemons this pokedex contains.
-	 * The list view will be sorted using the given <tt>order</tt>.
+	 * The list view will be sorted using the given <code>order</code>.
 	 * 
 	 * @param order Comparator instance used for sorting the created view.
 	 * @return Sorted unmodifiable list of all pokemons.
